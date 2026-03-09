f8ec055d81273a51a155e833a3f1751b62f91ec4
Aman singh <singhaman85867@gmail.com>
2026-03-09 18:57:11 +0530

    Fixed: Completely removed submodule and added as normal folder

---
+++ frontend/src/landing_page/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NotFound() {
+    return ( 
+         <div className='container p-5 mb-5'>
+        <div className='row text-center'>
+           <h1 clasName='mt-5'>404Not Found</h1>
+          <p>sorry, the page you are looking for does not exist.</p>
+          
+        </div>
+
+       </div>
+     );
+}
+
+export default NotFound;
---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+function Signup() {
+    return ( 
+        <div classNAme="container">
+           <h1>Signup</h1>
+        </div>
+     );
+}
+
+export default Signup;
